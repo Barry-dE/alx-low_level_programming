57285e9f581360f77cbb97a0063038067fb2c80a
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-09 16:03:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercased alphabets in reverse
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
