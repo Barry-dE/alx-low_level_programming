51fdef93a9163cac8067d2edac44c31a7de9fa1c
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-13 15:35:51 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet 10x.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; a++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_putchar(y);
+		}
+	_putchar('\n');
+	}
+}
