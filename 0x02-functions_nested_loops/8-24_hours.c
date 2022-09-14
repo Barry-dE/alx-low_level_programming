506140c845694164b0d3452a7de36d769d2a61d5
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-13 18:56:35 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,20 +12,21 @@ void jack_bauer(void)
 
 	for (a = 48; a <= 50; a++)
 	{
-
-		for (c = 48; c <= 53; c++)
+		for (b = 48; b <= 57; b++)
 		{
-			for (d = 48; d <= 57; d++)
+			for (c = 48; c <= 53; c++)
 			{
-				if (a >= 50 && b >= 52)
+				for (d = 48; d <= 57; d++)
 				{
-					break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
+					if (a >= 50 && b >= 52)
+					{
+						break;
+						_putchar(a);
+						_putchar(b);
+						_putchar(58);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
 				}
 			}
 		}
