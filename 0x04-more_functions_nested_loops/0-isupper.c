0afea394c08e888b12d1e667a1f068acee359ba0
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-15 14:15:43 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,15 @@
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 122)
 	{
-		return (1);
+		if (c == 67)
+		{
+			return (1);
+		}
 	}
 	else
 	{
-		retutn (0);
+		return (0);
 	}
 }
