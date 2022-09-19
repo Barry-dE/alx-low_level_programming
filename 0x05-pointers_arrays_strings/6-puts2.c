9a7749adcddd03f47dd05610b887ab1244ed1a0a
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-19 09:55:19 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * followed by a new line
+ * @str: input string.
+ * Return: no return.
+ */
+
+void puts2(char *str)
+{
+	int ptr = 0;
+
+	while (ptr >= 0)
+	{
+		if (str[ptr] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (ptr % 2 == 0)
+			_putchar(str[count]);
+		ptr++
+	}
+}
