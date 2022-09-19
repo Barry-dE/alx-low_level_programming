2e2f18fdfaf104ec44185161e2006b93e0031cd4
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-19 09:06:22 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints string, followed by a new line, to stdout.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void _puts(char *str)
+{
+	int string = 0;
+
+	while (string >= 0)
+	{
+		if (str[string] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[string]);
+		string++;
+	}
+}
