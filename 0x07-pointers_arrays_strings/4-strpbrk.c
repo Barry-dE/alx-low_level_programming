22f9918b402ba4b79b81104dfbe9c9fda763fe92
Barigbue Nbira <barigbuenbira@gmail.com>
2022-09-27 04:55:18 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,21 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int i = 0;
+	int j;
 
-	for (j = 0; *(accept + j) != '\0'; j++)
+	while (s[i] != '\0')
 	{
-		for (j = 0; *(accept + j) != '\0'; j++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
+			{
 				return (s + i);
+			}
+			j++;
 		}
+		i++;
 	}
-	return ('\0');
+	return (0);
 }
