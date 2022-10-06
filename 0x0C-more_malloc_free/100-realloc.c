5345e8dc905070f713dbf9312b0d83dbc0de254b
Barigbue Nbira <barigbuenbira@gmail.com>
2022-10-06 10:56:23 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,20 +15,39 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr)
+		free(ptr);
 		return (NULL);
 	}
-
+	p = ptr;
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+	{
+		return (NULL);
+	}
 	if (ptr == NULL)
-		ptr = malloc(new_size);
-
-	if (new_size == old_size)
-		return (ptr);
-
+	{
+		return (nptr);
+	}
+	if (new_size > old_size)
+	{
+		for (c = 0; c < old_size; c++)
+		{
+			nptr[c] = p[c];
+		}
+	}
+	else
+	{
+		for (c = 0; c < new_size; c++)
+		{
+			nptr[c] = p[c];
+		}
+	}
 	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	return (nptr);
 }
