1ac56acaa02e2e16f00aaf6caa3cbfd7d704682c
Barigbue Nbira <barigbuenbira@gmail.com>
2022-10-05 17:05:39 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,30 +10,38 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int x = 0, money = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	money = atoi(argv[1]);
+	while (money > 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		if (money - 25 >= 0)
+		{
+			money = money - 25;
+		}
+		else if (money - 10 >= 0)
+		{
+			money = money - 10;
+		}
+		else if (money - 5 >= 0)
+		{
+			money = money - 5;
+		}
+		else if (money - 2 >= 0)
+		{
+			money = money - 2;
+		}
+		else if (money - 1 == 0)
+		{
+			money = money - 1;
+		}
+		x++;
 	}
-	printf("%d\n", ncoins);
+	printf("%d\n", x);
 	return (0);
 }
