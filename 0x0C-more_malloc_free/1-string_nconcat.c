39b1ee42f4c9331f72b0eef720299a0d66a1a2b2
Barigbue Nbira <barigbuenbira@gmail.com>
2022-10-07 03:41:30 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * string_nconcat - concatenates two strings
  *
  * @s1: first string
  * @s2: second string
- * @n: amount of chars touse of second string
+ * @size: amount of chars touse of second string
  *
  * Return: a pointer to the concatenated string or NULL if the process fails
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int size)
 {
-	unsigned int i_s1, i_s2, len1, len2;
-	char *a;
+	unsigned int dist1, dist2;
 
-	if (s1 == NULL)
-		s1 = "";
+	char *ptr;
 
-	if (s2 == NULL)
-		s2 = "";
+	if (str1 == NULL)
+		str1 = "";
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
+	if (str2 == NULL)
+		str2 = "";
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
+	dist1 = strlen(str1);
+	dist2 = strlen(str2) <= size ? strlen(str2) : size;
+	ptr = malloc(dist1 + dist2 = 1);
 
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (ptr == NULL)
 		return (NULL);
+	strcpy(ptr, str1);
+	strncat(ptr, str2, size);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-
-	sout[i] = '\0';
-
-	return (sout);
+	return (ptr);
 }
