9b93cc8adc910daec5614149e1dc01fa7794813e
Barigbue Nbira <barigbuenbira@gmail.com>
2022-10-11 12:16:55 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef  _DOG_H
-#define  _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - creates a struct for dogs' information
@@ -9,19 +9,19 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-int _putchar(char c);
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
